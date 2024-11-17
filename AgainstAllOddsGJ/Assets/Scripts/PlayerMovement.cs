b9c70e4da889a8492efd13bc547565cd1f8082f6
mckennyjp <jumbomax792@gmail.com>
2024-11-17 16:41:02 -0600

    Final game jam prototype finished

--- AgainstAllOddsGJ/Assets/Scripts/PlayerMovement.cs
+++ AgainstAllOddsGJ/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
     public Rigidbody2D rb;
+    public Animator animator;
 
     private Vector2 moveDirection;
 
@@ -15,6 +16,7 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         ProcessInputs();
+
 
         if (Input.GetKeyDown("space"))
         {
@@ -34,6 +36,10 @@ public class PlayerMovement : MonoBehaviour
         float moveY = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector2(moveX, moveY).normalized;
+
+        animator.SetFloat("Horizontal", moveX);
+        animator.SetFloat("Vertical", moveY);
+        animator.SetFloat("Speed", moveDirection.sqrMagnitude);
     }
     void Move()
     {
